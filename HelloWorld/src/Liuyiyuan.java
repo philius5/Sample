fd97415f2b5f8c43e752cb30793992ebe146653f
philius5 <philius5@naver.com>
2015-06-22 14:04:05 +0900

    Automation Deployment test in codeship

--- HelloWorld/src/Liuyiyuan.java
+++ HelloWorld/src/Liuyiyuan.java
@@ -7,7 +7,7 @@ public class Liuyiyuan {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("Modify!!!!!");
+		System.out.println("Fully Automation!!!!!");
 	}
 
 }
